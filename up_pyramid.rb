8e46a670f7a3a5a95c8414b641875ada0a4cb23b
Hiroki-SH <srymc75@gmail.com>
2021-07-08 23:09:14 +0900

    上向きピラミッドのコードを簡略化

--- up_pyramid.rb
+++ up_pyramid.rb
@@ -1,6 +1,4 @@
 steps_num = ARGV[0].to_i
 steps_num.times do |n|
-  char_num = n * 2 + 1
-  step = "*" *  char_num
-  puts step.center(steps_num * 2 - 1)
+  puts ("*" * (n * 2 + 1)).center(steps_num * 2 - 1)
 end
